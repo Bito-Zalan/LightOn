1471c2980e973adb5d069567e029e7261268020f
Bito-Zalan <bitozalan23@gmail.com>
2024-04-17 20:37:05 +0200

    lighton html,css kialakitva

---
+++ index.js
@@ -0,0 +1,26 @@
+function lampaNemEg(){
+
+}
+
+function lekapcsotltakSzama(lista){
+    let szamol = 0;
+    for (let index = 0; index < 10; index++) {
+        if (lista[i] == false) {
+            szamol += 1;
+        }
+        return szamol
+    }
+}
+
+function lekapcsoltKiir(szamol) {
+    const LAMPA = lekapcsolva;
+    console.log(szamol);
+}
+
+function palyaKialakitas() {
+    const LISTA = [];
+    for (let index = 0; index < 9; index++) {
+        
+        
+    }
+}
